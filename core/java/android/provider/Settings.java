326ade4bda80975769767252164698171cb0291b
Lars Greiss <kufikugel@googlemail.com>
2015-02-12 21:12:10 +0500

    IME feature: Force to show always enter key [1/2]
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Change-Id: I06c5b7e4cbcddfe9d09ce2de6b2cea08ea3ddee9
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4093,6 +4093,12 @@ public final class Settings {
         public static final String DIALER_OPENCNAM_AUTH_TOKEN = "dialer_opencnam_auth_token";
 
         /**
+         * Forces formal text input.  1 to replace emoticon key with enter key.
+         * @hide
+         */
+        public static final String FORMAL_TEXT_INPUT = "formal_text_input";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
