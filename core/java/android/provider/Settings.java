4876a14ac090beec2c82d89e79ebb6d497c5d02c
INT3NSE07 <d.jonathanpaul@gmail.com>
2017-07-23 10:32:51 +0530

    Merge branch 'nougat-mr2' of https://github.com/AOSPA/android_frameworks_base into HEAD

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4142,6 +4142,13 @@ public final class Settings {
          */
         public static final String FORMAL_TEXT_INPUT = "formal_text_input";
 
+	/**
+         * Controls whether statusbar double tap to sleep is enabled
+         * @hide
+         */
+        public static final String GESTURE_DOUBLE_TAP_SLEEP = "gesture_double_tap_sleep";
+
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
